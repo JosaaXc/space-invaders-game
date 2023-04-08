27fc6e63180e08d759c077304ab8437742581949
JosaaXC <114438741+JosaaXc@users.noreply.github.com>
2023-04-07 20:29:58 -0500

    Invasores se mueven y chocan

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class Player{
             x: 0,
             y: 0
         }
+        this.rotation = 0
 
         const image = new Image()
         image.src = './img/spaceship.png'
@@ -30,12 +31,27 @@ class Player{
         //c.fillStyle = 'red'
         //c.fillRect(this.position.x, this.position.y, this.width,
             //this.height)
+
+        c.save()
+        c.translate(
+            player.position.x + player.width / 2, 
+            player.position.y + player.height / 2
+            )
+        c.rotate(this.rotation)
+        c.translate(
+            -player.position.x - player.width / 2, 
+            -player.position.y - player.height / 2
+            )
+
         c.drawImage(this.image,
             this.position.x,
             this.position.y,
             this.width,
             this.height
             )
+
+        
+        c.restore()
     }
 
     update(){
@@ -45,32 +61,201 @@ class Player{
         }
     }
 }
+class Projectile {
+    constructor({ position, velocity }){
+        this.position = position
+        this.velocity = velocity
 
+        this.radius = 3
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0,
+            Math.PI * 2)
+            c.fillStyle='red'
+            c.fill()
+            c.closePath()
+    }
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+class Invader {
+    constructor({ position }){
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = './img/invader.png'
+        image.onload = () => {
+            const scale = 1
+            this.image = image
+            this.width= image.width * scale
+            this.height= image.height * scale
+            this.position = {
+                x: position.x, 
+                y: position.y
+            }
+        }
+    }
+
+
+    draw(){
+        //c.fillStyle = 'red'
+        //c.fillRect(this.position.x, this.position.y, this.width,
+            //this.height)
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+            )
+    }
+
+    update({ velocity }){
+        if(this.image){
+        this.draw()
+        this.position.x += velocity.x;
+        this.position.y += velocity.y;
+        }
+    }
+}
+
+class Grid{
+    constructor(){
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.velocity = {
+            x: 7, 
+            y: 0
+        }
+        this.invaders = []
+        const columns = Math.floor(Math.random() * 10 + 5)
+        const rows = Math.floor(Math.random() * 5 + 2)
+
+        this.width = columns * 30
+
+        for(let x = 0; x < columns; x++){
+            for(let y = 0; y < rows; y++){
+                this.invaders.push(
+                   new Invader({
+                    position: {
+                        x: x * 30,
+                        y: y * 30
+                    }
+                    })
+                )
+            }
+        }
+    }
+    update(){
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+            this.velocity.x = -this.velocity.x;
+            this.velocity.y = 0;
+        }
+    }
+}
 const player = new Player()
+const projectiles = [] 
+const grids = [new Grid()]
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 
 function animate(){
     requestAnimationFrame(animate)
     c.fillStyle= 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    projectiles.forEach((projectile, index) => {
+        if(projectile.position.y + projectile.radius <= 0){
+            setTimeout(() =>{
+                projectiles.splice(index, 1)
+            }, 0)
+        }else{
+            projectile.update()
+        }
+    })
+
+    grids.forEach((grid) =>{
+        grid.update()
+        grid.invaders.forEach((invader) => {
+            invader.update({ velocity: grid.velocity })
+        })
+    })
+
+    if( keys.a.pressed && player.position.x>= 0){
+        player.velocity.x= -7
+        player.rotation = -0.15
+    }else if(keys.d.pressed && player.position.x + player.width <= canvas.width){
+        player.velocity.x= 7
+        player.rotation = 0.15
+    }else{
+        player.velocity.x= 0
+        player.rotation= 0
+    }
 }   
 
 animate()
 
-addEventListener('kewydown', ({key}) => {
-    console.log(key)
+addEventListener('keydown', ({ key }) => {
     switch(key){
         case 'a':
-            console.log('left')
+            player.velocity.x = -5
+            keys.a.pressed = true
             break
         case 'd':
-            console.log('right')
+            keys.d.pressed = true
             break
         case ' ':
-            console.log('space')
+            //console.log('space')
+            keys.space.pressed = true
+            projectiles.push(
+                new Projectile({
+                    position: {
+                        x: player.position.x + player.width / 2,
+                        y: player.position.y 
+                    },
+                    velocity: {
+                        x: 0,
+                        y: -15
+                    }
+                }))
             break
-        case 's':
-            console.log('down')
-            break  
+    }
+})
+
+addEventListener('keyup', ({ key }) => {
+    switch(key){
+        case 'a':
+            player.velocity.x = -5
+            keys.a.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+        case ' ':
+            keys.space.pressed = true
+            break
     }
 })
